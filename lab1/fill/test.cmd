dad5305814ac8e7d8abdb0b4b63c9f9dbe68c812
m3tro1d <qazwf734@gmail.com>
2022-02-11 19:18:57 +0300

    lab1 fill: add tests for file access errors

--- lab1/fill/test.cmd
+++ lab1/fill/test.cmd
@@ -12,28 +12,36 @@ rem Launching with incorrect argument count returns non-zero exit code
 %SUBJECT% tests\regular.txt > nul 2> nul && goto failed
 echo Test 1 passed
 
+rem Providing non-existing input file results in an error
+%SUBJECT% tests\non-existing.txt %OUTPUT% > nul 2> nul && goto failed
+echo Test 2 passed
+
+rem Providing unavailable output file results in an error
+%SUBJECT% tests\regular.txt Y:\output.txt > nul 2> nul && goto failed
+echo Test 3 passed
+
 rem Providing invalid field input results in an error
 %SUBJECT% tests\invalid.txt %OUTPUT% > nul 2> nul && goto failed
-echo Test 2 passed
+echo Test 4 passed
 
 rem Input containing no marker results in an error
 %SUBJECT% tests\marker-less.txt %OUTPUT% > nul 2> nul && goto failed
-echo Test 3 passed
+echo Test 5 passed
 
 rem Filling the provided input works correctly
 %SUBJECT% tests\regular.txt %OUTPUT% > nul 2> nul || goto failed
 fc tests\regular-output.txt %OUTPUT% > nul || goto failed
-echo Test 4 passed
+echo Test 6 passed
 
 rem Field above 100x100 area is ignored
 %SUBJECT% tests\large-field.txt %OUTPUT% > nul 2> nul || goto failed
 fc tests\large-field-output.txt %OUTPUT% > nul 2> nul || goto failed
-echo Test 5 passed
+echo Test 7 passed
 
 rem If the marker is placed outside, all surrounding area is filled
 %SUBJECT% tests\who-spilled-the-bucket.txt %OUTPUT% > nul 2> nul || goto failed
 fc tests\who-spilled-the-bucket-output.txt %OUTPUT% > nul 2> nul || goto failed
-echo Test 6 passed
+echo Test 8 passed
 
 exit /B 0
 
