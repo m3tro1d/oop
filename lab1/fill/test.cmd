0a9965f84edd6f7a8de98f0431dd6bf6aa095a81
m3tro1d <qazwf734@gmail.com>
2022-02-11 19:08:26 +0300

    lab1 fill: add tests

--- lab1/fill/test.cmd
+++ lab1/fill/test.cmd
@@ -16,10 +16,24 @@ rem Providing invalid field input results in an error
 %SUBJECT% tests\invalid.txt %OUTPUT% > nul 2> nul && goto failed
 echo Test 2 passed
 
+rem Input containing no marker results in an error
+%SUBJECT% tests\marker-less.txt %OUTPUT% > nul 2> nul && goto failed
+echo Test 3 passed
+
 rem Filling the provided input works correctly
 %SUBJECT% tests\regular.txt %OUTPUT% > nul 2> nul || goto failed
 fc tests\regular-output.txt %OUTPUT% > nul || goto failed
-echo Test 3 passed
+echo Test 4 passed
+
+rem Field above 100x100 area is ignored
+%SUBJECT% tests\large-field.txt %OUTPUT% > nul 2> nul || goto failed
+fc tests\large-field-output.txt %OUTPUT% > nul 2> nul || goto failed
+echo Test 5 passed
+
+rem If the marker is placed outside, all surrounding area is filled
+%SUBJECT% tests\who-spilled-the-bucket.txt %OUTPUT% > nul 2> nul || goto failed
+fc tests\who-spilled-the-bucket-output.txt %OUTPUT% > nul 2> nul || goto failed
+echo Test 6 passed
 
 exit /B 0
 
