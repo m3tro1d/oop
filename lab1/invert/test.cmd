804c6caf8ed0ebcb272ae2ece2572663faadff27
m3tro1d <qazwf734@gmail.com>
2022-02-11 19:20:04 +0300

    lab1 invert: add tests for file access errors

--- lab1/invert/test.cmd
+++ lab1/invert/test.cmd
@@ -15,17 +15,22 @@ rem Providing non-existing matrix file results in an error
 %SUBJECT% tests\non-existing.txt > nul 2> nul && goto failed
 echo Test 2 passed
 
+rem Providing unavailable output file results in an error
+%SUBJECT% tests\regular.txt Y:\output.txt > nul 2> nul && goto failed
+echo Test 3 passed
+
 rem Providing invalid matrix file results in an error
 %SUBJECT% tests\invalid-matrix-format.txt > nul 2> nul && goto failed
-echo Test 3 passed
+echo Test 4 passed
 
 rem Trying to find inverse of a zero-determined matrix results in an error
 %SUBJECT% tests\zero-determinant.txt > nul 2> nul && goto failed
-echo Test 4 passed
+echo Test 5 passed
 
+rem Regular matrix (invertible) is processed correctly
 %SUBJECT% tests\regular.txt > %OUTPUT% 2> nul || goto failed
 fc tests\regular-output.txt %OUTPUT% > nul || goto failed
-echo Test 5 passed
+echo Test 6 passed
 
 exit /B 0
 
