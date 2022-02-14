6329ab3f3828e723699722f2e7f58d33bd961332
m3tro1d <qazwf734@gmail.com>
2022-02-14 18:25:52 +0300

    lab1 fill: add read-only output test

--- lab1/fill/test.cmd
+++ lab1/fill/test.cmd
@@ -27,30 +27,35 @@ rem Providing unavailable output file results in an error
 fc tests\unavailable-output.txt %OUTPUT% > nul || goto failed
 echo Test 3 passed
 
+rem Existing but not available for writing output file results in an error
+%SUBJECT% tests\regular.txt %SUBJECT% > %OUTPUT% 2>&1 && goto failed
+fc tests\not-writable-output.txt %OUTPUT% > nul || goto failed
+echo Test 4 passed
+
 rem Providing invalid field input results in an error
 %SUBJECT% tests\invalid.txt %TRASH% > %OUTPUT% 2>&1 && goto failed
 fc tests\invalid-output.txt %OUTPUT% > nul || goto failed
-echo Test 4 passed
+echo Test 5 passed
 
 rem Input containing no marker results in an error
 %SUBJECT% tests\marker-less.txt %TRASH% > %OUTPUT% 2>&1 && goto failed
 fc tests\marker-less-output.txt %OUTPUT% > nul || goto failed
-echo Test 5 passed
+echo Test 6 passed
 
 rem Filling the provided input works correctly
 %SUBJECT% tests\regular.txt %OUTPUT% > nul 2>&1 || goto failed
 fc tests\regular-output.txt %OUTPUT% > nul || goto failed
-echo Test 6 passed
+echo Test 7 passed
 
 rem Field above 100x100 area is ignored
 %SUBJECT% tests\large-field.txt %OUTPUT% > nul 2>&1 || goto failed
 fc tests\large-field-output.txt %OUTPUT% > nul 2>&1 || goto failed
-echo Test 7 passed
+echo Test 8 passed
 
 rem If the marker is placed outside, all surrounding area is filled
 %SUBJECT% tests\who-spilled-the-bucket.txt %OUTPUT% > nul 2>&1 || goto failed
 fc tests\who-spilled-the-bucket-output.txt %OUTPUT% > nul 2>&1 || goto failed
-echo Test 8 passed
+echo Test 9 passed
 
 echo.
 echo All tests passed
