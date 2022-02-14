a2c186f1dce8e1e08cc5892110c90e25af42f5f6
m3tro1d <qazwf734@gmail.com>
2022-02-14 20:32:12 +0300

    lab1 crypt: add more test cases

--- lab1/crypt/test.cmd
+++ lab1/crypt/test.cmd
@@ -6,6 +6,7 @@ if %SUBJECT% == "" (
 	goto error
 )
 set OUTPUT="%TEMP%\output.txt"
+set BIN_OUTPUT="%TEMP%\output.bin"
 set TRASH="%TEMP%\%RANDOM%.txt"
 
 rem Launching with incorrect argument count returns non-zero exit code
@@ -43,14 +44,42 @@ fc tests\invalid-key-value-output.txt %OUTPUT% > nul || goto failed
 echo Test 4 passed
 
 rem Providing non-existing input file results in an error
+%SUBJECT% crypt tests\non-existing.jpg %TRASH% 42 > %OUTPUT% 2>&1 && goto failed
+fc tests\non-existing-output.txt %OUTPUT% > nul || goto failed
+echo Test 5 passed
 
 rem Providing unavailable output file results in an error
+%SUBJECT% crypt tests\regular.jpg Y:\output.bin 42 > %OUTPUT% 2>&1 && goto failed
+fc tests\unavailable-output.txt %OUTPUT% > nul || goto failed
+echo Test 6 passed
 
 rem Existing but not available for writing output file results in an error
+%SUBJECT% crypt tests\regular.jpg %SUBJECT% 42 > %OUTPUT% 2>&1 && goto failed
+fc tests\not-writable-output.txt %OUTPUT% > nul || goto failed
+echo Test 7 passed
 
 rem Providing empty input file results in an empty output in both encryption and decryption
+%SUBJECT% crypt tests\empty.bin %BIN_OUTPUT% 42 > nul 2>&1 || goto failed
+fc /b tests\empty-output.bin %BIN_OUTPUT% > nul || goto failed
+
+%SUBJECT% decrypt tests\empty-output.bin %BIN_OUTPUT% 42 > nul 2>&1 || goto failed
+fc /b tests\empty.bin %BIN_OUTPUT% > nul || goto failed
+
+echo Test 8 passed
 
 rem Normal encryption and decryption works correctly
+%SUBJECT% crypt tests\regular.jpg %BIN_OUTPUT% 42 > nul 2>&1 || goto failed
+fc /b tests\regular-42-output.bin %BIN_OUTPUT% > nul || goto failed
+
+%SUBJECT% decrypt tests\regular-42-output.bin %BIN_OUTPUT% 42 > nul 2>&1 || goto failed
+fc /b tests\regular.jpg %BIN_OUTPUT% > nul || goto failed
+
+echo Test 9 passed
+
+rem Trying to decrypt file encrypted with different key doesn't produce the same file
+%SUBJECT% decrypt tests\regular-42-output.bin %BIN_OUTPUT% 13 > nul 2>&1 || goto failed
+fc /b tests\regular.jpg %BIN_OUTPUT% > nul 2>&1 && goto failed
+echo Test 10 passed
 
 echo.
 echo All tests passed
