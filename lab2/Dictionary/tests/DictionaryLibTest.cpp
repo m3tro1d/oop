3dbedf62f00d653ed4f4ac557f247974eb9d3840
m3tro1d <qazwf734@gmail.com>
2022-02-24 22:04:12 +0300

    lab2 Dictionary: add tests stubs

--- lab2/Dictionary/tests/DictionaryLibTest.cpp
+++ lab2/Dictionary/tests/DictionaryLibTest.cpp
@@ -1,4 +1,32 @@
 #define CATCH_CONFIG_MAIN
+#include "../DictionaryLib.h"
 #include "catch.hpp"
 
-// TODO
+TEST_CASE("dictionary file operations work correctly")
+{
+	SECTION("dictionary file path is determined correctly")
+	{
+		SECTION("dictionary file path grabs value from the arguments if they are present")
+		{
+			// TODO
+		}
+
+		SECTION("dictionary file path fallbacks to the default value if incorrect arguments were provided")
+		{
+			int const argc = 1;
+			auto const dictionaryPath = GetDictionaryPath(argc, nullptr);
+
+			REQUIRE(dictionaryPath == DEFAULT_DICTIONARY_PATH);
+		}
+	}
+
+	SECTION("dictionary file initial parsing works correctly")
+	{
+		// TODO
+	}
+
+	SECTION("dictionary file saving works correctly")
+	{
+		// TODO
+	}
+}
