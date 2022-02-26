b112cdb05f44b5dbf49b93410e550d6eb79d48db
m3tro1d <qazwf734@gmail.com>
2022-02-26 11:59:29 +0300

    lab2 Dictionary: slight formatting

--- lab2/Dictionary/DictionaryFileLib.h
+++ lab2/Dictionary/DictionaryFileLib.h
@@ -11,7 +11,11 @@ const std::string DEFAULT_DICTIONARY_PATH = "dictionary.txt";
 std::string tolower(const std::string& string);
 
 std::string GetDictionaryPath(int argc, char** argv);
+
 void ReadDictionaryFile(std::istream& dictionaryFile, Dictionary& dictionary);
+
 void LoadDictionary(const std::string& dictionaryPath, Dictionary& dictionary);
+
 void WriteDictionaryFile(std::ostream& dictionaryFile, const Dictionary& dictionary);
+
 void SaveDictionary(const std::string& dictionaryFilename, const Dictionary& dictionary);
--- lab2/Dictionary/DictionaryLib.h
+++ lab2/Dictionary/DictionaryLib.h
@@ -9,4 +9,5 @@
 using Dictionary = std::map<std::string, std::string>;
 
 std::optional<std::string> LookupTranslation(const Dictionary& dictionary, const std::string& phrase);
+
 void AddTranslation(Dictionary& dictionary, const std::string& phrase, const std::string& translation);
