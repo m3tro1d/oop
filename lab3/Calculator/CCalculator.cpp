2d69e4c732a0f2f6ae99ce43b80ba33a75ace204
m3tro1d <qazwf734@gmail.com>
2022-03-08 10:27:39 +0300

    lab3 Calculator: remove tokenizer, add new class outline

--- lab3/Calculator/CCalculator.cpp
+++ lab3/Calculator/CCalculator.cpp
@@ -1,17 +1,3 @@
 #include "CCalculator.h"
 
-/*
- * Tokenize the input, then engage token processor
- *
- * Example:
- *
- * let a = 2
- * let b = 3
- * let c = a + b
- * print c
- *
- * ASSIGNMENT IDENTIFIER NUMBER END
- * ASSIGNMENT IDENTIFIER NUMBER END
- * ASSIGNMENT IDENTIFIER IDENTIFIER PLUS IDENTIFIER END
- * PRINT IDENTIFIER END
- */
+// TODO
--- lab3/Calculator/CCalculator.h
+++ lab3/Calculator/CCalculator.h
@@ -1,5 +1,37 @@
 #pragma once
 
+#include <limits>
+#include <map>
+#include <string>
+
 class CCalculator
 {
+public:
+	using Identifier = std::string;
+	using Value = double;
+	using Expression = std::string;
+
+	using Variables = std::map<Identifier, Value>;
+	using Functions = std::map<Identifier, Expression>;
+
+	static constexpr Value NON_EXISTING_VALUE = std::numeric_limits<Value>::signaling_NaN();
+
+public:
+	void CreateVariable(const Identifier& identifier);
+	void AssignVariable(const Identifier& identifier, const Expression& value);
+
+	void CreateFunction(const Identifier& identifier, const Expression& expression);
+
+	Value CalculateValueByIdentifier(const Identifier& identifier);
+
+	Variables DumpVariables() const;
+	Functions DumpFunctions() const;
+
+private:
+	bool IsValidIdentifier(const Identifier& identifier) const;
+	bool DoesIdentifierExist(const Identifier& identifier) const;
+
+private:
+	Variables m_variables;
+	Functions m_functions;
 };
