bc2f0b81f130a9b243f9d8d0d06428cf4e096904
m3tro1d <qazwf734@gmail.com>
2022-03-16 19:58:30 +0300

    lab4 Figures: implement basic rectangle

---
+++ lab4/Figures/Shapes/SolidShapes/Rectangle/CRectangle.cpp
@@ -0,0 +1,54 @@
+#include "CRectangle.h"
+
+CRectangle::CRectangle(CPoint leftTop, CPoint rightBottom, uint32_t outlineColor, uint32_t fillColor)
+	: m_leftTop(leftTop)
+	, m_rightBottom(rightBottom)
+	, m_outlineColor(outlineColor)
+	, m_fillColor(fillColor)
+{
+}
+
+double CRectangle::GetArea() const
+{
+	return GetWidth() * GetHeight();
+}
+
+double CRectangle::GetPerimeter() const
+{
+	return 2 * (GetWidth() + GetHeight());
+}
+
+std::string CRectangle::ToString() const
+{
+	return "Rectangle";
+}
+
+uint32_t CRectangle::GetOutlineColor() const
+{
+	return m_outlineColor;
+}
+
+uint32_t CRectangle::GetFillColor() const
+{
+	return m_fillColor;
+}
+
+CPoint CRectangle::GetLeftTop() const
+{
+	return m_leftTop;
+}
+
+CPoint CRectangle::GetRightBottom() const
+{
+	return m_rightBottom;
+}
+
+double CRectangle::GetWidth() const
+{
+	return m_rightBottom.GetX() - m_leftTop.GetX();
+}
+
+double CRectangle::GetHeight() const
+{
+	return m_leftTop.GetY() - m_rightBottom.GetY();
+}
---
+++ lab4/Figures/Shapes/SolidShapes/Rectangle/CRectangle.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../../../Point/CPoint.h"
+#include "../ISolidShape.h"
+
+class CRectangle : public ISolidShape
+{
+public:
+	CRectangle(CPoint leftTop, CPoint rightBottom, uint32_t outlineColor, uint32_t fillColor);
+
+	double GetArea() const override;
+	double GetPerimeter() const override;
+	std::string ToString() const override;
+	uint32_t GetOutlineColor() const override;
+
+	uint32_t GetFillColor() const override;
+
+	CPoint GetLeftTop() const;
+	CPoint GetRightBottom() const;
+
+	double GetWidth() const;
+	double GetHeight() const;
+
+private:
+	CPoint m_leftTop;
+	CPoint m_rightBottom;
+
+	uint32_t m_outlineColor;
+	uint32_t m_fillColor;
+};
