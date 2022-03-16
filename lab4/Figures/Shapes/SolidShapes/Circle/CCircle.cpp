0db694f508403f15f41125bca30993a1b8d7c59a
m3tro1d <qazwf734@gmail.com>
2022-03-16 19:52:03 +0300

    lab4 Figures: first circle implementation

---
+++ lab4/Figures/Shapes/SolidShapes/Circle/CCircle.cpp
@@ -0,0 +1,44 @@
+#include "CCircle.h"
+
+CCircle::CCircle(CPoint center, double radius, uint32_t outlineColor, uint32_t fillColor)
+	: m_center(center)
+	, m_radius(radius)
+	, m_outlineColor(outlineColor)
+	, m_fillColor(fillColor)
+{
+}
+
+double CCircle::GetArea() const
+{
+	return M_PI * m_radius * m_radius;
+}
+
+double CCircle::GetPerimeter() const
+{
+	return 2 * M_PI * m_radius;
+}
+
+std::string CCircle::ToString() const
+{
+	return "Circle";
+}
+
+uint32_t CCircle::GetOutlineColor() const
+{
+	return m_outlineColor;
+}
+
+uint32_t CCircle::GetFillColor() const
+{
+	return m_fillColor;
+}
+
+CPoint CCircle::GetCenter() const
+{
+	return m_center;
+}
+
+double CCircle::GetRadius() const
+{
+	return m_radius;
+}
