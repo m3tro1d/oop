f5049b523da4debcf2f2c248fcde984e79a74c34
m3tro1d <qazwf734@gmail.com>
2022-03-16 19:44:44 +0300

    lab4 Figures: add and implement point class

---
+++ lab4/Figures/Point/CPoint.cpp
@@ -0,0 +1,17 @@
+#include "CPoint.h"
+
+CPoint::CPoint(double x, double y)
+	: m_x(x)
+	, m_y(y)
+{
+}
+
+double CPoint::GetX() const
+{
+	return m_x;
+}
+
+double CPoint::GetY() const
+{
+	return m_y;
+}
