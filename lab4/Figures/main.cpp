91a56e0334ae3b1203881c2dd1de2c1dd95637a9
m3tro1d <qazwf734@gmail.com>
2022-03-16 20:29:13 +0300

    lab4 Figures: add task outline

--- lab4/Figures/main.cpp
+++ lab4/Figures/main.cpp
@@ -3,5 +3,11 @@
 int main()
 {
 	// TODO
+	//  1. Parse figures from stdin in a shape vector
+	//  2. Find figure having the biggest area
+	//  3. Find figure having the smallest perimeter
+	//  4. Print these figures: area and perimeter,
+	//     outline and fill colors, specific figure data
+
 	return EXIT_SUCCESS;
 }
