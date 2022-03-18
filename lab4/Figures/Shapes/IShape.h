4861387b6baae2e7d9f5f47bb90c48bf8af316d9
m3tro1d <qazwf734@gmail.com>
2022-03-18 15:06:42 +0300

    lab4 Figures: add TODO for refactoring

--- lab4/Figures/Shapes/IShape.h
+++ lab4/Figures/Shapes/IShape.h
@@ -8,7 +8,7 @@ public:
 	virtual double GetArea() const = 0;
 	virtual double GetPerimeter() const = 0;
 	virtual std::string ToString() const = 0;
-	virtual uint32_t GetOutlineColor() const = 0;
+	virtual uint32_t GetOutlineColor() const = 0; // TODO: encapsulate working with outline and fill colors in an abstract class
 
 	virtual ~IShape() = default;
 };
