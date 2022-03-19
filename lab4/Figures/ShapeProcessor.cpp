fb2b7ad49eb2f73199b37fa27901674cc9228401
m3tro1d <qazwf734@gmail.com>
2022-03-19 14:26:37 +0300

    lab4 Figures: implement shape processor

--- lab4/Figures/ShapeProcessor.cpp
+++ lab4/Figures/ShapeProcessor.cpp
@@ -1,4 +1,5 @@
 #include "ShapeProcessor.h"
+#include "Shapes/LineSegment/CLineSegment.h"
 
 ShapeProcessor::ShapeProcessor(std::istream& input, std::ostream& output)
 	: m_input(input)
@@ -20,8 +21,161 @@ void ShapeProcessor::ProcessShapes()
 
 ShapeProcessor::ShapeVector ShapeProcessor::ReadShapes()
 {
-	// TODO
-	return {};
+	ShapeVector result;
+	while (!m_input.eof())
+	{
+		auto const command = ReadCommand();
+		auto shape = GetHandlerForCommand(command.type)(command.arguments);
+		if (shape)
+		{
+			result.push_back(std::move(shape));
+		}
+	}
+
+	return result;
+}
+
+ShapeProcessor::Command ShapeProcessor::ReadCommand()
+{
+	std::string userInput;
+	std::string arguments;
+	std::getline(m_input, userInput);
+	if (userInput.empty())
+	{
+		return {
+			.type = CommandType::IDLE,
+		};
+	}
+
+	std::stringstream input(userInput);
+	CommandType type;
+	std::string commandTypeStr;
+	std::getline(input, commandTypeStr, ' ');
+	if (!std::getline(input, arguments))
+	{
+		throw std::invalid_argument("no arguments provided");
+	}
+	type = ParseCommandType(commandTypeStr);
+
+	return {
+		.type = type,
+		.arguments = arguments,
+	};
+}
+
+ShapeProcessor::CommandType ShapeProcessor::ParseCommandType(const std::string& command)
+{
+	auto const type = COMMAND_KEYWORDS.find(command);
+	if (type == COMMAND_KEYWORDS.end())
+	{
+		throw std::invalid_argument("invalid command");
+	}
+
+	return type->second;
+}
+
+ShapeProcessor::CommandHandler ShapeProcessor::GetHandlerForCommand(ShapeProcessor::CommandType type)
+{
+	switch (type)
+	{
+	case CommandType::LINE:
+		return [](const std::string& arguments) {
+			return CreateLine(arguments);
+		};
+	case CommandType::CIRCLE:
+		return [](const std::string& arguments) {
+			return CreateCircle(arguments);
+		};
+	case CommandType::RECTANGLE:
+		return [](const std::string& arguments) {
+			return CreateRectangle(arguments);
+		};
+	case CommandType::TRIANGLE:
+		return [](const std::string& arguments) {
+			return CreateTriangle(arguments);
+		};
+	default:
+		return [](const std::string&) {
+			return nullptr;
+		};
+	}
+}
+
+ShapeProcessor::Shape ShapeProcessor::CreateLine(const std::string& arguments)
+{
+	std::stringstream input(arguments);
+	auto const start = ReadPoint(input);
+	auto const end = ReadPoint(input);
+	auto const outlineColor = ReadColor(input);
+
+	return std::make_unique<CLineSegment>(start, end, outlineColor);
+}
+
+ShapeProcessor::Shape ShapeProcessor::CreateCircle(const std::string& arguments)
+{
+	std::stringstream input(arguments);
+	auto const center = ReadPoint(input);
+	auto const radius = ReadValue(input);
+	auto const outlineColor = ReadColor(input);
+	auto const fillColor = ReadColor(input);
+
+	return std::make_unique<CCircle>(center, radius, outlineColor, fillColor);
+}
+
+ShapeProcessor::Shape ShapeProcessor::CreateRectangle(const std::string& arguments)
+{
+	std::stringstream input(arguments);
+	auto const topLeft = ReadPoint(input);
+	auto const width = ReadValue(input);
+	auto const height = ReadValue(input);
+	auto const outlineColor = ReadColor(input);
+	auto const fillColor = ReadColor(input);
+
+	return std::make_unique<CRectangle>(topLeft, width, height, outlineColor, fillColor);
+}
+
+ShapeProcessor::Shape ShapeProcessor::CreateTriangle(const std::string& arguments)
+{
+	std::stringstream input(arguments);
+	auto const vertex1 = ReadPoint(input);
+	auto const vertex2 = ReadPoint(input);
+	auto const vertex3 = ReadPoint(input);
+	auto const outlineColor = ReadColor(input);
+	auto const fillColor = ReadColor(input);
+
+	return std::make_unique<CTriangle>(vertex1, vertex2, vertex3, outlineColor, fillColor);
+}
+
+CPoint ShapeProcessor::ReadPoint(std::istream& input)
+{
+	double x = ReadValue(input);
+	double y = ReadValue(input);
+
+	return { x, y };
+}
+
+uint32_t ShapeProcessor::ReadColor(std::istream& input)
+{
+	uint32_t value;
+
+	if (!(input >> std::hex >> value))
+	{
+		throw std::invalid_argument("invalid arguments");
+	}
+
+	return value;
+}
+
+double ShapeProcessor::ReadValue(std::istream& input)
+{
+	double value;
+
+	if (!(input >> value))
+	{
+		throw std::invalid_argument("invalid arguments");
+	}
+
+	return value;
 }
 
 void ShapeProcessor::PrintShapeWithLargestArea(const ShapeProcessor::ShapeVector& shapes)
@@ -32,6 +186,8 @@ void ShapeProcessor::PrintShapeWithLargestArea(const ShapeProcessor::ShapeVector
 		[](const Shape& shape1, const Shape& shape2) {
 			return shape1->GetArea() < shape2->GetArea();
 		});
+
+	m_output << "\n[Largest area shape]\n";
 	PrintShapeInfo(*largestAreaShape);
 }
 
@@ -43,12 +199,15 @@ void ShapeProcessor::PrintShapeWithSmallestPerimeter(const ShapeProcessor::Shape
 		[](const Shape& shape1, const Shape& shape2) {
 			return shape1->GetPerimeter() < shape2->GetPerimeter();
 		});
+
+	m_output << "\n[Smallest perimeter shape]\n";
 	PrintShapeInfo(*smallestPerimeterShape);
 }
 
 void ShapeProcessor::PrintShapeInfo(const Shape& shape)
 {
-	m_output << shape->ToString()
-			 << "area: " << shape->GetArea() << '\n'
-			 << "perimeter: " << shape->GetPerimeter() << '\n';
+	m_output << std::fixed << std::setprecision(PRINT_PRECISION)
+			 << shape->ToString()
+			 << "  area: " << shape->GetArea() << '\n'
+			 << "  perimeter: " << shape->GetPerimeter() << '\n';
 }
--- lab4/Figures/ShapeProcessor.h
+++ lab4/Figures/ShapeProcessor.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "Point/CPoint.h"
 #include "Shapes/IShape.h"
+#include "Shapes/SolidShapes/Circle/CCircle.h"
+#include "Shapes/SolidShapes/Rectangle/CRectangle.h"
+#include "Shapes/SolidShapes/Triangle/CTriangle.h"
+#include <functional>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <sstream>
+#include <string>
 #include <vector>
 
 class ShapeProcessor
@@ -13,10 +21,48 @@ public:
 	void ProcessShapes();
 
 private:
+	static constexpr int PRINT_PRECISION = 2;
+
 	using Shape = std::unique_ptr<IShape>;
 	using ShapeVector = std::vector<Shape>;
 
+	enum class CommandType
+	{
+		IDLE,
+		LINE,
+		CIRCLE,
+		RECTANGLE,
+		TRIANGLE,
+	};
+
+	struct Command
+	{
+		CommandType type;
+		std::string arguments;
+	};
+
+	using CommandHandler = std::function<Shape(const std::string& arguments)>;
+
+	const std::map<std::string, CommandType> COMMAND_KEYWORDS = {
+		{ "line", CommandType::LINE },
+		{ "circle", CommandType::CIRCLE },
+		{ "rectangle", CommandType::RECTANGLE },
+		{ "triangle", CommandType::TRIANGLE },
+	};
+
 	ShapeVector ReadShapes();
+	Command ReadCommand();
+	CommandType ParseCommandType(const std::string& command);
+	static CommandHandler GetHandlerForCommand(CommandType type);
+
+	static Shape CreateLine(const std::string& arguments);
+	static Shape CreateCircle(const std::string& arguments);
+	static Shape CreateRectangle(const std::string& arguments);
+	static Shape CreateTriangle(const std::string& arguments);
+
+	static CPoint ReadPoint(std::istream& input);
+	static uint32_t ReadColor(std::istream& input);
+	static double ReadValue(std::istream& input);
 
 	void PrintShapeWithLargestArea(const ShapeVector& shapes);
 	void PrintShapeWithSmallestPerimeter(const ShapeVector& shapes);
