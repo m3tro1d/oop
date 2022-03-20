e4657d3c278581e507abecaaae1b5bb5b4ee2035
m3tro1d <qazwf734@gmail.com>
2022-03-20 10:48:27 +0300

    lab4 Figures: add canvas interfaces

---
+++ lab4/Figures/Drawing/ICanvas.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Point/CPoint.h"
+#include <cstdint>
+#include <vector>
+
+class ICanvas
+{
+public:
+	virtual ~ICanvas() = default;
+
+	virtual void DrawLine(CPoint from, CPoint to, uint32_t lineColor) = 0;
+	virtual void DrawPolygon(std::vector<CPoint> points, uint32_t outlineColor, uint32_t fillColor) = 0;
+	virtual void DrawCircle(CPoint center, double radius, uint32_t lineColor, uint32_t fillColor) = 0;
+};
---
+++ lab4/Figures/Drawing/ICanvasDrawable.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ICanvas.h"
+
+class ICanvasDrawable
+{
+public:
+	virtual ~ICanvasDrawable() = default;
+
+	virtual void Draw(ICanvas& canvas) const = 0;
+};
