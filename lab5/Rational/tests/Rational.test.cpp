e7aba07dbac3fdc6fa460e25fbf8f9b43b1e36f5
m3tro1d <qazwf734@gmail.com>
2022-03-21 21:23:24 +0300

    lab5 Rational: remake tests with TDD

--- lab5/Rational/tests/Rational.test.cpp
+++ lab5/Rational/tests/Rational.test.cpp
@@ -2,58 +2,73 @@
 #include "../CRational.h"
 #include "catch.hpp"
 
-SCENARIO("creating a rational number")
+bool ApproximatelyEquals(double a, double b)
 {
-	GIVEN("nothing")
+	return std::abs(a - b) < std::numeric_limits<double>::epsilon();
+}
+
+TEST_CASE("creating a rational number")
+{
+	SECTION("empty number")
 	{
 		CRational r;
 
-		THEN("numerator is 0")
-		{
-			REQUIRE(r.GetNumerator() == 0);
-		}
-
-		THEN("denominator is 1")
-		{
-			REQUIRE(r.GetDenominator() == 1);
-		}
+		REQUIRE(r.GetNumerator() == 0);
+		REQUIRE(r.GetDenominator() == 1);
 	}
 
-	GIVEN("a number")
+	SECTION("with a single number")
 	{
 		int number = 42;
 		CRational r = number;
 
-		THEN("numerator equals to that number")
-		{
-			REQUIRE(r.GetNumerator() == number);
-		}
-
-		THEN("denominator is 1")
-		{
-			REQUIRE(r.GetDenominator() == 1);
-		}
+		REQUIRE(r.GetNumerator() == number);
+		REQUIRE(r.GetDenominator() == 1);
 	}
 
-	GIVEN("numerator and denominator")
+	SECTION("with numerator and denominator")
 	{
 		int numerator = 42;
 		int denominator = 69;
 		CRational r(numerator, denominator);
 
-		THEN("numerator is the same")
-		{
-			REQUIRE(r.GetNumerator() == numerator);
-		}
-
-		THEN("denominator is the same")
-		{
-			REQUIRE(r.GetDenominator() == denominator);
-		}
+		REQUIRE(r.GetNumerator() == numerator);
+		REQUIRE(r.GetDenominator() == denominator);
 	}
 }
 
-SCENARIO("getting double representation of a rational number")
+TEST_CASE("getting double representation of a rational number")
 {
-	// TODO
+	SECTION("empty number")
+	{
+		CRational r;
+
+		REQUIRE(ApproximatelyEquals(r.ToDouble(), 0));
+	}
+
+	SECTION("with a single number")
+	{
+		int number = 42;
+		CRational r = number;
+
+		REQUIRE(ApproximatelyEquals(r.ToDouble(), number));
+	}
+
+	SECTION("with numerator and denominator")
+	{
+		int numerator = 42;
+		int denominator = 69;
+		CRational r(numerator, denominator);
+
+		REQUIRE(ApproximatelyEquals(r.ToDouble(), numerator / static_cast<double>(denominator)));
+	}
+
+	SECTION("with zero denominator")
+	{
+		int numerator = 42;
+		int denominator = 0;
+		CRational r(numerator, denominator);
+
+		REQUIRE_THROWS_AS(r.ToDouble(), std::logic_error);
+	}
 }
