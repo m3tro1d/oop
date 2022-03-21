02dfdc753ff3d0cbdd9ed7eba3cc1a15744287e7
m3tro1d <qazwf734@gmail.com>
2022-03-21 10:22:44 +0300

    lab5 Rational: add creation tests, leave TODO

--- lab5/Rational/tests/Rational.test.cpp
+++ lab5/Rational/tests/Rational.test.cpp
@@ -1,12 +1,59 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 #include "../CRational.h"
+#include "catch.hpp"
 
 SCENARIO("creating a rational number")
 {
 	GIVEN("nothing")
 	{
 		CRational r;
-		// TODO
+
+		THEN("numerator is 0")
+		{
+			REQUIRE(r.GetNumerator() == 0);
+		}
+
+		THEN("denominator is 1")
+		{
+			REQUIRE(r.GetDenominator() == 1);
+		}
 	}
+
+	GIVEN("a number")
+	{
+		int number = 42;
+		CRational r = number;
+
+		THEN("numerator equals to that number")
+		{
+			REQUIRE(r.GetNumerator() == number);
+		}
+
+		THEN("denominator is 1")
+		{
+			REQUIRE(r.GetDenominator() == 1);
+		}
+	}
+
+	GIVEN("numerator and denominator")
+	{
+		int numerator = 42;
+		int denominator = 69;
+		CRational r(numerator, denominator);
+
+		THEN("numerator is the same")
+		{
+			REQUIRE(r.GetNumerator() == numerator);
+		}
+
+		THEN("denominator is the same")
+		{
+			REQUIRE(r.GetDenominator() == denominator);
+		}
+	}
+}
+
+SCENARIO("getting double representation of a rational number")
+{
+	// TODO
 }
