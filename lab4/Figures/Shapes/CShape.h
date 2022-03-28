75a7ea117f6b8e1e53dcfd8274f83fe4c9c25e67
m3tro1d <qazwf734@gmail.com>
2022-03-28 09:35:58 +0300

    lab4 Figures: remove unnecessary virtual destructors
    These classes inherit from IShape, which already has a virtual destructor

--- lab4/Figures/Shapes/CShape.h
+++ lab4/Figures/Shapes/CShape.h
@@ -7,8 +7,6 @@ class CShape : public IShape
 public:
 	CShape(uint32_t outlineColor);
 
-	virtual ~CShape() = default;
-
 	uint32_t GetOutlineColor() const final;
 
 protected:
--- lab4/Figures/Shapes/SolidShapes/CSolidShape.h
+++ lab4/Figures/Shapes/SolidShapes/CSolidShape.h
@@ -8,8 +8,6 @@ class CSolidShape : public ISolidShape
 public:
 	CSolidShape(uint32_t outlineColor, uint32_t fillColor);
 
-	virtual ~CSolidShape() = default;
-
 	uint32_t GetOutlineColor() const final;
 	uint32_t GetFillColor() const final;
 
