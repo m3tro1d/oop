f03051263380f37e444d043eb01c261ce59fc4f8
m3tro1d <qazwf734@gmail.com>
2022-03-28 09:35:12 +0300

    lab4 Figures: remove ICanvasDrawable interface
    Not used anywhere

--- lab4/Figures/Shapes/IShape.h
+++ lab4/Figures/Shapes/IShape.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#include "../Drawing/ICanvasDrawable.h"
+#include "../Drawing/ICanvas.h"
 #include <string>
 
-class IShape : public ICanvasDrawable
+class IShape
 {
 public:
 	virtual double GetArea() const = 0;
 	virtual double GetPerimeter() const = 0;
 	virtual std::string ToString() const = 0;
 	virtual uint32_t GetOutlineColor() const = 0;
+
+	virtual void Draw(ICanvas& canvas) const = 0;
 
 	virtual ~IShape() = default;
 };
