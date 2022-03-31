e76871cccbcb212c06f3cc5710fff41fac2b9ec4
m3tro1d <qazwf734@gmail.com>
2022-03-31 21:39:55 +0300

    lab5 Rational: add operators signatures

--- lab5/Rational/CRational.h
+++ lab5/Rational/CRational.h
@@ -32,6 +32,33 @@ public:
 	CRational& operator-=(const CRational& other);
 	CRational& operator-=(const int& n);
 
+	friend CRational operator*(const CRational& r1, const CRational& r2);
+	friend CRational operator*(const CRational& r1, const int& n);
+	friend CRational operator*(const int& n, const CRational& r2);
+
+	friend CRational operator/(const CRational& r1, const CRational& r2);
+	friend CRational operator/(const CRational& r1, const int& n);
+	friend CRational operator/(const int& n, const CRational& r2);
+
+	CRational& operator*=(const CRational& other);
+	CRational& operator*=(const int& n);
+
+	CRational& operator/=(const CRational& other);
+	CRational& operator/=(const int& n);
+
+	friend bool operator==(const CRational& r1, const CRational& r2);
+	friend bool operator==(const CRational& r1, const int& n);
+	friend bool operator==(const int& n, const CRational& r2);
+
+	friend bool operator!=(const CRational& r1, const CRational& r2);
+	friend bool operator!=(const CRational& r1, const int& n);
+	friend bool operator!=(const int& n, const CRational& r2);
+
+	// TODO: < <= > >=
+
+	friend std::ostream& operator<<(std::ostream& stream, const CRational& r);
+	friend std::istream& operator>>(std::istream& stream, const CRational& r);
+
 private:
 	void Normalize();
 
