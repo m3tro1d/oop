35de1d8aa0c8c54da8c37741460fa601909055c7
m3tro1d <qazwf734@gmail.com>
2022-04-04 20:13:00 +0300

    lab5 MyString: rearrange tests, add stubs

--- lab5/MyString/tests/MyString.test.cpp
+++ lab5/MyString/tests/MyString.test.cpp
@@ -42,50 +42,66 @@ TEST_CASE("string creation")
 	}
 }
 
-TEST_CASE("string copying")
+TEST_CASE("string creation from other strings")
 {
-	SECTION("copy contains the same string with the same length")
+	SECTION("copying")
 	{
-		CMyString s = "Hello, wonderful World!";
-		CMyString copy(s);
+		SECTION("copy contains the same string with the same length")
+		{
+			CMyString s = "Hello, wonderful World!";
+			CMyString copy(s);
 
-		REQUIRE(std::strcmp(s.GetStringData(), copy.GetStringData()) == 0);
-		REQUIRE(s.GetLength() == copy.GetLength());
+			REQUIRE(std::strcmp(s.GetStringData(), copy.GetStringData()) == 0);
+			REQUIRE(s.GetLength() == copy.GetLength());
+		}
+
+		SECTION("modifying the copy does not affect the original string")
+		{
+			char const* initialString = "Hello, wonderful World!";
+			CMyString s = initialString;
+			CMyString copy(s);
+			char* firstCharPtr = const_cast<char*>(copy.GetStringData());
+			*firstCharPtr = 'B';
+
+			REQUIRE(std::strcmp(s.GetStringData(), initialString) == 0);
+			REQUIRE(std::strcmp(copy.GetStringData(), initialString) != 0);
+		}
 	}
 
-	SECTION("modifying the copy does not affect the original string")
+	SECTION("moving")
 	{
-		char const* initialString = "Hello, wonderful World!";
-		CMyString s = initialString;
-		CMyString copy(s);
-		char* firstCharPtr = const_cast<char*>(copy.GetStringData());
-		*firstCharPtr = 'B';
+		SECTION("moved string contains the same string with the same length")
+		{
+			char const* initialString = "Hello, wonderful World!";
+			size_t const initialLength = std::strlen(initialString);
+			CMyString s = initialString;
+			CMyString moved(std::move(s));
 
-		REQUIRE(std::strcmp(s.GetStringData(), initialString) == 0);
-		REQUIRE(std::strcmp(copy.GetStringData(), initialString) != 0);
+			REQUIRE(std::strcmp(initialString, moved.GetStringData()) == 0);
+			REQUIRE(initialLength == moved.GetLength());
+		}
+
+		SECTION("initial string is zeroed after moving")
+		{
+			CMyString s = "Hello, wonderful World!";
+			CMyString moved(std::move(s));
+
+			REQUIRE(s.GetStringData() == nullptr);
+			REQUIRE(s.GetLength() == 0);
+		}
 	}
 }
 
-TEST_CASE("string moving")
+TEST_CASE("string assignment")
 {
-	SECTION("moved string contains the same string with the same length")
+	SECTION("copy assignment")
 	{
-		char const* initialString = "Hello, wonderful World!";
-		size_t const initialLength = std::strlen(initialString);
-		CMyString s = initialString;
-		CMyString moved(std::move(s));
-
-		REQUIRE(std::strcmp(initialString, moved.GetStringData()) == 0);
-		REQUIRE(initialLength == moved.GetLength());
+		// TODO
 	}
 
-	SECTION("initial string is zeroed after moving")
+	SECTION("move assignment")
 	{
-		CMyString s = "Hello, wonderful World!";
-		CMyString moved(std::move(s));
-
-		REQUIRE(s.GetStringData() == nullptr);
-		REQUIRE(s.GetLength() == 0);
+		// TODO
 	}
 }
 
@@ -101,4 +117,54 @@ TEST_CASE("string clearing")
 	REQUIRE(true);
 }
 
-// TODO: operators
+TEST_CASE("string concatenation")
+{
+	SECTION("concatenation-assignment")
+	{
+		// TODO
+	}
+
+	SECTION("plain concatenation")
+	{
+		// TODO
+	}
+}
+
+TEST_CASE("string comparison")
+{
+	SECTION("equality")
+	{
+		// TODO
+	}
+
+	SECTION("inequality")
+	{
+		// TODO
+	}
+}
+
+TEST_CASE("lexicographical comparison")
+{
+	SECTION("strict: <, >")
+	{
+		// TODO
+	}
+
+	SECTION("not strict: <=, >=")
+	{
+		// TODO
+	}
+}
+
+TEST_CASE("string input-output using streams")
+{
+	SECTION("writing")
+	{
+		// TODO
+	}
+
+	SECTION("reading")
+	{
+		// TODO
+	}
+}
