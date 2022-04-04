5f2b09f03b484552f14fe05d734f84199db4e72f
m3tro1d <qazwf734@gmail.com>
2022-04-04 09:39:24 +0300

    lab5 MyString: add tests outline and string creation tests

--- lab5/MyString/tests/MyString.test.cpp
+++ lab5/MyString/tests/MyString.test.cpp
@@ -1,4 +1,58 @@
 #define CATCH_CONFIG_MAIN
+#include "../CMyString.h"
 #include "catch.hpp"
 
-// TODO
+TEST_CASE("string creation")
+{
+	SECTION("empty string")
+	{
+		CMyString s;
+		auto const stringData = s.GetStringData();
+
+		REQUIRE(s.GetLength() == 0);
+		REQUIRE(stringData[0] == '\0');
+	}
+
+	SECTION("using C string")
+	{
+		auto const cString = "Hello, wonderful World!";
+		CMyString s(cString);
+		auto const stringData = s.GetStringData();
+
+		REQUIRE(s.GetLength() == std::strlen(cString));
+		REQUIRE(std::strcmp(stringData, cString) == 0);
+	}
+
+	SECTION("using char pointer and length")
+	{
+		auto const cString = "Hello, wonderful World!";
+		int const length = 5;
+		CMyString s(cString, length);
+		auto const stringData = s.GetStringData();
+
+		REQUIRE(s.GetLength() == length);
+		REQUIRE(std::strcmp(stringData, "Hello") == 0);
+	}
+
+	SECTION("using STL string")
+	{
+		std::string const stlString = "Hello, wonderful World of C++ standard library!";
+		CMyString s(stlString);
+		auto const stringData = s.GetStringData();
+
+		REQUIRE(s.GetLength() == stlString.length());
+		REQUIRE(stringData == stlString);
+	}
+}
+
+TEST_CASE("string copying")
+{
+	// TODO
+	REQUIRE(true);
+}
+
+TEST_CASE("string moving")
+{
+	// TODO
+	REQUIRE(true);
+}
