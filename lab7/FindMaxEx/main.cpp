2363018bbf924e68ec68fddc88b84682bd73bda9
m3tro1d <qazwf734@gmail.com>
2022-05-14 14:22:25 +0300

    lab7 FindMaxEx: implement the main program

--- lab7/FindMaxEx/main.cpp
+++ lab7/FindMaxEx/main.cpp
@@ -1,29 +1,114 @@
 #include "FindMaxEx.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
 struct Sportsman
 {
 	std::string firstName;
 	std::string lastName;
 	std::string patronymic;
-	int height;
-	int weight;
+
+	int height = 0;
+	int weight = 0;
 };
 
-Sportsman FindSportsmanWithMaxHeight(std::vector<Sportsman> const& sportsmen);
-Sportsman FindSportsmanWithMaxWeight(std::vector<Sportsman> const& sportsmen);
+std::optional<std::string> GetSportsmenFilename(int argc, char** argv);
+void InitializeFile(std::ifstream& file, std::string const& filename);
+std::vector<Sportsman> ParseSportsmen(std::istream& input);
+Sportsman FindTallestSportsman(std::vector<Sportsman> const& sportsmen);
+Sportsman FindHeaviestSportsman(std::vector<Sportsman> const& sportsmen);
+void PrintSportsman(std::ostream& output, Sportsman const& sportsman);
 
-int main()
+int main(int argc, char** argv)
 {
-	// TODO
+	auto const sportsmenFilename = GetSportsmenFilename(argc, argv);
+	if (!sportsmenFilename)
+	{
+		std::cerr << "Invalid argument count\n"
+				  << "Usage: FindMaxEx.exe <sportsmen file>\n";
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		std::ifstream sportsmenFile;
+		InitializeFile(sportsmenFile, sportsmenFilename.value());
+		auto const sportsmen = ParseSportsmen(sportsmenFile);
+
+		auto const tallestSportsman = FindTallestSportsman(sportsmen);
+		auto const heaviestSportsman = FindHeaviestSportsman(sportsmen);
+
+		std::cout << "[Tallest Sportsman]\n";
+		PrintSportsman(std::cout, tallestSportsman);
+		std::cout << "\n[Heaviest Sportsman]\n";
+		PrintSportsman(std::cout, heaviestSportsman);
+	}
+	catch (std::exception const& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
 
-Sportsman FindSportsmanWithMaxHeight(std::vector<Sportsman> const& sportsmen)
+std::optional<std::string> GetSportsmenFilename(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		return std::nullopt;
+	}
+
+	return argv[1];
+}
+
+void InitializeFile(std::ifstream& file, std::string const& filename)
+{
+	file.open(filename);
+	if (!file.is_open())
+	{
+		throw std::runtime_error("failed to open input file for reading");
+	}
+}
+
+std::vector<Sportsman> ParseSportsmen(std::istream& input)
+{
+	std::vector<Sportsman> result;
+
+	std::string line;
+	while (std::getline(input, line))
+	{
+		std::stringstream lineStream(line);
+		Sportsman sportsman;
+		lineStream >> sportsman.lastName >> sportsman.firstName >> sportsman.patronymic
+			>> sportsman.height
+			>> sportsman.weight;
+
+		if (lineStream.bad())
+		{
+			throw std::runtime_error("invalid sportsmen file format");
+		}
+
+		result.push_back(std::move(sportsman));
+	}
+
+	return result;
+}
+
+Sportsman FindTallestSportsman(std::vector<Sportsman> const& sportsmen)
 {
 	Sportsman result;
-	auto isEmpty = !FindMaxEx(sportsmen, result, [](Sportsman const& s1, Sportsman const& s2) {
-		return s1.height < s2.height;
-	});
+	auto isEmpty = !FindMaxEx(
+		sportsmen,
+		result,
+		[](Sportsman const& s1, Sportsman const& s2) {
+			return s1.height < s2.height;
+		});
 
 	if (isEmpty)
 	{
@@ -33,12 +118,15 @@ Sportsman FindSportsmanWithMaxHeight(std::vector<Sportsman> const& sportsmen)
 	return result;
 }
 
-Sportsman FindSportsmanWithMaxWeight(std::vector<Sportsman> const& sportsmen)
+Sportsman FindHeaviestSportsman(std::vector<Sportsman> const& sportsmen)
 {
 	Sportsman result;
-	auto isEmpty = !FindMaxEx(sportsmen, result, [](Sportsman const& s1, Sportsman const& s2) {
-		return s1.weight < s2.weight;
-	});
+	auto isEmpty = !FindMaxEx(
+		sportsmen,
+		result,
+		[](Sportsman const& s1, Sportsman const& s2) {
+			return s1.weight < s2.weight;
+		});
 
 	if (isEmpty)
 	{
@@ -46,4 +134,11 @@ Sportsman FindSportsmanWithMaxWeight(std::vector<Sportsman> const& sportsmen)
 	}
 
 	return result;
+}
+
+void PrintSportsman(std::ostream& output, Sportsman const& sportsman)
+{
+	output << sportsman.lastName << " " << sportsman.firstName << " " << sportsman.patronymic << "\n"
+		   << "Height: " << sportsman.height << "\n"
+		   << "Weight: " << sportsman.weight << "\n";
 }
