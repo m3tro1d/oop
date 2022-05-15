63465e77940d63c40ab98c288cdd3ec8946c3855
m3tro1d <qazwf734@gmail.com>
2022-05-15 21:44:52 +0300

    lab7 MyArray: add sample program

--- lab7/MyArray/main.cpp
+++ lab7/MyArray/main.cpp
@@ -1,8 +1,26 @@
 #include "CMyArray.hpp"
 #include <iostream>
+#include <string>
 
 int main()
 {
-	// TODO
+	CMyArray<int> intArray;
+	intArray.Push(1);
+	intArray.Push(2);
+	intArray.Push(3);
+	for (auto const& element : intArray)
+	{
+		std::cout << element << '\n';
+	}
+
+	CMyArray<std::string> stringArray;
+	stringArray.Push("Bitterness and burden");
+	stringArray.Push("Curses rest on thee");
+	stringArray.Push("Solitaire and sorrow");
+	for (auto const& element : stringArray)
+	{
+		std::cout << element << '\n';
+	}
+
 	return EXIT_SUCCESS;
 }
